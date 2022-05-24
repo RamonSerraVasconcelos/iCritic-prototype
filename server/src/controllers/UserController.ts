aec784ddc39a9251507de99b7df2b63420f23dd0
RamonSerraVasconcelos <rserravasconcelos@gmail.com>
2022-05-24 20:08:39 -0300

    Settings up first route

---
+++ server/src/controllers/UserController.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from 'express';
+
+const UserController = {
+    register(req: Request, res: Response) {
+        return res.send();
+    }
+}
+
+export default UserController;
