8ad2d7e45f52d95c6b4859a4ae9081bb048c2c3b
RamonSerraVasconcelos <rserravasconcelos@gmail.com>
2022-08-27 22:55:15 -0300

    Creating update endpoint and logic

--- server/src/middlewares/validators/movie-validator.ts
+++ server/src/middlewares/validators/movie-validator.ts
@@ -1,11 +1,4 @@
-import {
-    checkEmptyRequestBody,
-    isEmpty,
-    checkEmptyValue,
-    hasValueMinLength,
-    validateDate,
-    validateNumber,
-} from '@src/utils/validations';
+import { checkEmptyRequestBody, isEmpty, checkEmptyValue, hasValueMinLength, validateDate, validateNumber } from '@src/utils/validations';
 import { Request, Response, NextFunction } from 'express';
 import ResponseError from '@src/ts/classes/response-error';
 
@@ -16,16 +9,9 @@ const validateCreation = (req: Request, res: Response, next: NextFunction) => {
         checkEmptyValue(req.body[key]);
     });
 
-    const { name, synopsis, releaseDate, country, language, directorId } =
-        req.body;
+    const { name, synopsis, releaseDate, country, language, directorId } = req.body;
 
-    if (
-        isEmpty(name) ||
-        isEmpty(synopsis) ||
-        isEmpty(releaseDate) ||
-        isEmpty(country) ||
-        isEmpty(language)
-    ) {
+    if (isEmpty(name) || isEmpty(synopsis) || isEmpty(releaseDate) || isEmpty(country) || isEmpty(language)) {
         throw new ResponseError('Missing fields in request', 400);
     }
 
@@ -34,14 +20,19 @@ const validateCreation = (req: Request, res: Response, next: NextFunction) => {
     }
 
     if (!hasValueMinLength(synopsis, 5)) {
-        throw new ResponseError(
-            'Synopsis must contain 5 characters minimum',
-            400,
-        );
+        throw new ResponseError('Synopsis must contain 5 characters minimum', 400);
     }
 
     if (!validateDate(releaseDate)) {
         throw new ResponseError('Invalid release date', 400);
+    }
+
+    if (!validateNumber(country)) {
+        throw new ResponseError('Incorret value for country id', 400);
+    }
+
+    if (!validateNumber(language)) {
+        throw new ResponseError('Incorret value for language id', 400);
     }
 
     if (!validateNumber(directorId)) {
@@ -51,4 +42,44 @@ const validateCreation = (req: Request, res: Response, next: NextFunction) => {
     next();
 };
 
-export default validateCreation;
+const validateUpdate = (req: Request, res: Response, next: NextFunction) => {
+    checkEmptyRequestBody(req);
+
+    Object.keys(req.body).forEach((key) => {
+        checkEmptyValue(req.body[key]);
+    });
+
+    const { id, name, synopsis, releaseDate, country, language, directorId } = req.body;
+
+    if (isEmpty(id) || !hasValueMinLength(id, 24)) {
+        throw new ResponseError('Invalid Id format', 400);
+    }
+
+    if (name && !hasValueMinLength(name, 1)) {
+        throw new ResponseError('Name must contain 1 character minimum', 400);
+    }
+
+    if (synopsis && !hasValueMinLength(synopsis, 5)) {
+        throw new ResponseError('Synopsis must contain 5 characters minimum', 400);
+    }
+
+    if (releaseDate && !validateDate(releaseDate)) {
+        throw new ResponseError('Invalid release date', 400);
+    }
+
+    if (country && !validateNumber(country)) {
+        throw new ResponseError('Incorret value for country id', 400);
+    }
+
+    if (language && !validateNumber(language)) {
+        throw new ResponseError('Incorret value for language id', 400);
+    }
+
+    if (directorId && !validateNumber(directorId)) {
+        throw new ResponseError('Incorret value for director id', 400);
+    }
+
+    next();
+};
+
+export { validateCreation, validateUpdate };
