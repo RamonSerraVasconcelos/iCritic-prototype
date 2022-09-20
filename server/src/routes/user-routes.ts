74c5d6054920dafa1a85ec384f9c2f1506ae53f8
RamonSerraVasconcelos <rserravasconcelos@gmail.com>
2022-09-19 22:38:10 -0300

    Creating profilePic endpoint

--- server/src/routes/user-routes.ts
+++ server/src/routes/user-routes.ts
@@ -3,13 +3,22 @@ import { validateRegister, validateUserUpdate, validateUserGet } from '@src/midd
 import tryCatch from '@src/utils/try-catch';
 import { userAuth } from '@src/middlewares/jwt/verify-token';
 import roles from '@src/middlewares/validators/role-validator';
+import multer from 'multer';
+import { multerConfig } from '@src/middlewares/files/multerConfig';
 import userController from '@src/controllers/user-controller';
 
 const routes = Router();
 
 routes.post('/register', validateRegister, tryCatch(userController.register));
-routes.put('/:id', userAuth, roles('USER'), validateUserUpdate, tryCatch(userController.update));
-routes.get('/', userAuth, roles('ADMIN'), tryCatch(userController.list));
-routes.get('/:id', userAuth, roles('USER'), validateUserGet, tryCatch(userController.get));
+
+/**
+ * Middleware to authenticate requests.
+ */
+routes.use(userAuth);
+
+routes.put('/:id', roles('USER'), validateUserUpdate, tryCatch(userController.update));
+routes.get('/', roles('ADMIN'), tryCatch(userController.list));
+routes.get('/:id', roles('USER'), validateUserGet, tryCatch(userController.get));
+routes.post('/profilePic', roles('USER'), multer(multerConfig).single('file'), tryCatch(userController.updateProfilePic));
 
 export default routes;
