dc50bffbcd1fd75b6fd373155862ece17902f16d
Ramon Vasconcelos <rserravasconcelos@gmail.com>
2023-01-10 21:27:42 -0300

    Fixing roles check logic

--- server/src/middlewares/roles-middleware.ts
+++ server/src/middlewares/roles-middleware.ts
@@ -6,12 +6,14 @@ import { tryCatch } from '@src/utils/try-catch';
 export const roles = (...allowedRoles: Role[]) => {
     return tryCatch(async (req: Request, res: Response, next: NextFunction) => {
         const rolesArray = [...allowedRoles];
-
         const userRole = req.user.role;
-        if (!userRole) throw new ResponseError('No role specified!', 401);
+
+        if (!userRole) throw new ResponseError('User has no system role!', 403);
+
+        if (userRole === Role.ADMIN) return next();
 
         const isAllowed = rolesArray.includes(userRole);
-        if (!isAllowed) throw new ResponseError('Unauthorized!', 401);
+        if (!isAllowed) throw new ResponseError('', 403);
 
         return next();
     });
