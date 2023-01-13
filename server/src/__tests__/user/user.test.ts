6fa529c4424cdc7f86f9acece178b8003c245453
Ramon Vasconcelos <rserravasconcelos@gmail.com>
2023-01-12 23:06:16 -0300

    Creating get user test and using random generator for user

--- server/src/__tests__/user/user.test.ts
+++ server/src/__tests__/user/user.test.ts
@@ -1,6 +1,7 @@
 import { exec } from 'child_process';
 import supertest from 'supertest';
 import { prisma } from '@src/lib/prisma';
+import generator from '@src/utils/random-generator';
 import app from '../../app';
 
 describe('user', () => {
@@ -13,23 +14,63 @@ describe('user', () => {
 
     describe('register user', () => {
         it('should create user and return his data', async () => {
-            const data = {
-                email: 'create@test.test',
-                name: 'Test User',
-                description: 'Testing user',
-                countryId: 29,
-                password: '12345678',
-            };
-
+            const userData = await generator.getRandomUser();
+            userData.password = '12345678';
             await supertest(app)
                 .post(`/register`)
-                .send(data)
+                .send(userData)
                 .expect(201)
                 .then(async (res) => {
                     expect(res.body.user.id).toBeTruthy();
                     expect(res.body.user.name).toBeTruthy();
                     expect(res.body.user.email).toBeTruthy();
                     expect(res.body.user.description).toBeTruthy();
+                });
+        });
+    });
+
+    describe('get user', () => {
+        it('should return user info', async () => {
+            const userData = await generator.getRandomUser();
+
+            await prisma.user.create({
+                data: {
+                    email: userData.email,
+                    name: userData.name,
+                    description: userData.description,
+                    countryId: userData.countryId,
+                    password: userData.password,
+                },
+            });
+
+            const data = {
+                email: userData.email,
+                password: '12345678',
+            };
+
+            let accessToken = '';
+
+            await supertest(app)
+                .post(`/login`)
+                .send(data)
+                .expect(200)
+                .then(async (res) => {
+                    accessToken = res.body.accessToken;
+                });
+
+            await supertest(app)
+                .get(`/users/1`)
+                .set('Authorization', `Bearer ${accessToken}`)
+                .expect(200)
+                .then(async (res) => {
+                    expect(res.body.user.id).toBeTruthy();
+                    expect(res.body.user.name).toBeTruthy();
+                    expect(res.body.user.email).toBeTruthy();
+                    expect(res.body.user.description).toBeTruthy();
+                    expect(res.body.user.role).toBeTruthy();
+                    expect(res.body.user.active).toBeTruthy();
+                    expect(res.body.user.countryId).toBeTruthy();
+                    expect(res.body.user.createdAt).toBeTruthy();
                 });
         });
     });
